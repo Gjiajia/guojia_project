410032823a41bc2fd51d36e4ab81c4fc57c9513f
Gjiajia <1062019650@qq.com>
2017-05-27 18:03:24 +0800

    version1
    上传原型

---
+++ files/修订记录/data.js
@@ -0,0 +1,36 @@
+﻿$axure.loadCurrentPage({
+  "url":"修订记录.html",
+  "generationDate":new Date(1495879052126.51),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"c0374db81ae342628578164c869ba227",
+    "type":"Axure:Page",
+    "name":"修订记录",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFF333333},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
